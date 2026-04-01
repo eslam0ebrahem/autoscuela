7ef4b16c82c04437ef8ef2671b6da1992a7ae013
islam ibrahim <eslam0ebrahem0@gmail.com>
2026-04-01 03:42:45 +0200

    chore: cleanup unnecessary files and update .gitignore

--- instrumentation.js
+++ instrumentation.js
@@ -1,6 +1,1 @@
-export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
-    const { startKeepAlive } = await import('./lib/keep-alive')
-    startKeepAlive()
-  }
-}
+export async function register() {}
